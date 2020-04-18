70b1d2ed046ef4e63250e76aaa3dac967dfb7339
CodeChomper <Jason@CodeChomper.com>
2020-04-18 09:52:12 -0700

    Farmer walking in all four directions

--- Scripts/Farmer.gd
+++ Scripts/Farmer.gd
@@ -4,9 +4,12 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var SPEED = 100
+const SPEED = 120
+const UP = Vector2(0,-1)
 var anim
-
+var motion = Vector2(0,0)
+var fliped = false
+var up = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim = $Animations
@@ -16,17 +19,37 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_pressed("ui_left")):
-		anim.play("Walk")
-		anim.set_flip_h(false)
-		position[0] -= SPEED * delta
-		position[1] += (SPEED/2) * delta
+		fliped = true
+		up = true
+		anim.play("WalkUp")
+		motion.x = -SPEED
+		motion.y = (-SPEED/2)
 	elif(Input.is_action_pressed("ui_right")):
+		fliped = true
+		up = false
 		anim.play("Walk")
-		anim.set_flip_h(true)
-		position[0] += SPEED * delta
-		position[1] += (SPEED/2) * delta
+		motion.x = SPEED
+		motion.y = (SPEED/2)
+	elif(Input.is_action_pressed("ui_up")):
+		fliped = false
+		up = true
+		anim.play("WalkUp")
+		motion.x = SPEED
+		motion.y = (-SPEED/2)
+	elif(Input.is_action_pressed("ui_down")):
+		fliped = false
+		up = false
+		anim.play("Walk")
+		motion.x = -SPEED
+		motion.y = (SPEED/2)
 	else:
-		anim.play("Idle")
-		
+		if(up):
+			anim.play("IdleUp")
+		else:
+			anim.play("Idle")
+		motion.x = 0
+		motion.y = 0
+	motion = move_and_slide(motion,UP)
+	anim.set_flip_h(fliped)
 		
 	pass
